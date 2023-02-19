4397b9de449abeee236c3d48114baef38a5bbd88
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-19 09:51:25 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    get circumference() {
+        return Math.PI * this.diameter;
+    }
+
+    get area() {
+        return Math.PI * this.radius**2;
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2;
+    }
+
+    set circumference(diameter) {
+        this.radius = (diameter / 2) / Math.PI;
+    }
+
+    set area(diameter) {
+        this.radius = Math.sqrt(diameter / Math.PI);
+    }
+}
